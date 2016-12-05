921e760bb7c0db07f6a1a1dea72b3203b7a92db9
Petter Andersson <tna12pan@student.lu.se>
2016-12-05 14:13:14 +0100

    Update ClientMain.java

--- EDA040-Camera/src/client/ClientMain.java
+++ EDA040-Camera/src/client/ClientMain.java
@@ -7,12 +7,10 @@ public class ClientMain {
 
     public static void main(String[] args) {
         Model model = new Model();
-        ReaderThread rt = new ReaderThread(model);
         SignalingThread st = new SignalingThread(model);
         st.start();
         GUI gui = new GUI(model);
         ImageProcessingThread IPT = new ImageProcessingThread(model, gui);
-        rt.start();
         IPT.start();
     }
 }
